0aa61ed86aaa151c67ca17f7c4d7ad6e7df5ca77
Kasper <Kasper@192.168.1.52>
2017-02-14 14:59:17 +0100

    Refactored

--- src/test1/Test1.java
+++ src/test1/Test1.java
@@ -12,17 +12,54 @@ import javax.swing.JOptionPane;
  * @author Kasper
  */
 public class Test1 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, test1(Integer.parseInt(JOptionPane.showInputDialog("Enter first (x) value")), Integer.parseInt(JOptionPane.showInputDialog("Enter second (y) value")), Integer.parseInt(JOptionPane.showInputDialog("Enter third (z) value"))));
+        JOptionPane.showMessageDialog(null, test1());
     }
-    
-    public static String test1(int x, int y, int z){
-        String result;
+    public static String test1(){
+        String result = "";
+        boolean run = true;
+        String xs = "";
+        String ys = "";
+        String zs = "";
+        
+        while(run){
+            xs = JOptionPane.showInputDialog("Enter first (x) value");
+            if(xs.matches("^-?\\d+$") && !xs.isEmpty() && Integer.parseInt(xs) > 0){
+                run = false;
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Invalid input! Must be a whole number above 0");
+            }
+        }
+        run = true;
+        while(run){
+            ys = JOptionPane.showInputDialog("Enter second (y) value");
+            if(ys.matches("^-?\\d+$") && !ys.isEmpty() && Integer.parseInt(ys) > 0){
+                run = false;
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Invalid input! Must be a whole number above 0");
+            }
+        }
+        run = true;
+        while(run){
+            zs = JOptionPane.showInputDialog("Enter third (z) value");
+            if(zs.matches("^-?\\d+$") && !zs.isEmpty() && Integer.parseInt(zs) > 0){
+                run = false;
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Invalid input! Must be a whole number above 0");
+            }
+        }
+        
+        int x = Integer.parseInt(xs);
+        int y = Integer.parseInt(ys);
+        int z = Integer.parseInt(zs);
 
+        if (x + y > z && x + z > y && y + z > x){
         if(x == y && y == z){
             result = "equilateral";
         }
@@ -32,6 +69,10 @@ public class Test1 {
         else{
             result = "scalene";
         }   
+        }
+        else{
+            result = "Inputs are not a valid triangle";
+        }
         return result;
     }
     
